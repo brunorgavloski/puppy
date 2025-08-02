e9361a7e80bf375783d108b99e8128a0a79dc382
Bruno <gavloski11@gmail..com>
2025-08-02 20:16:17 -0300

    asd

--- puppy.go
+++ puppy.go
@@ -1,9 +1,20 @@
 package puppy
 
+import (
+	"github.com/brunorgavloski/dog"
+)
+
 func Bark() string {
 	return "woof!"
 }
 
-func Barks() string {
-	return "woof! woof! woof! woof!"
+func Bark() string {
+	return dog.WhenGrowUp(Bark())
+}
+func BigBark() string {
+	return dog.WhenGrowUp(Bark())
+}
+
+func BigBark() string {
+	return dog.WhenGrowUp(Bark())
 }
